86b4b6637e6b04d4f5f73ce27db7e93937167f3a
yang_bing <test49@test49.com>
2018-10-17 17:31:24 +0800

    新增页面

--- src/main.js
+++ src/main.js
@@ -3,10 +3,10 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import { Button, Row, Col ,Search , Swipe , SwipeItem  } from 'vant'
+Vue.use(Button).use(Row).use(Col).use(Search).use(Swipe).use(SwipeItem)
+Vue.config.productionTip = false;
 
-Vue.config.productionTip = false
-
-/* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
